5a7fa70c61159574a9a16808bde4e91b171999f3
qdenisq <qdenisq@gmail.com>
2018-11-25 18:09:24 +1100

    DS: bug: need to normalize output

--- main.py
+++ main.py
@@ -26,44 +26,56 @@ def plot_gmm(dists):
     # data = np.random.multivariate_normal([0, 0], [[5, 2], [2, 2]], size=2000)
     data = pd.DataFrame(data, columns=['x0', 'x1'])
     with sns.axes_style('white'):
-        sns.jointplot("x0", "x1", data, kind='kde');
+        sns.jointplot("x0", "x1", data, kind='kde', title='weights');
 
 
 def propagate(dists, x0, num_outputs):
+    x1 = torch.zeros(num_outputs)
+
     x1_idx = torch.linspace(0, 1, num_outputs)
     x0_idx = torch.linspace(0, 1, x0.shape[0])
 
-    x1 = x1_idx.repeat(5)
+
 
     x0v, x1v = torch.meshgrid([x0_idx, x1_idx])
     points = torch.stack([x0v, x1v], dim=2)
-    points = points.view(-1, 2)
-    # log probs
-    log_probs = dists.log_prob(points)
+    # points = points.view(-1, 2)
+    for j in range(x1_idx.shape[0]):
+        for i in range(x0_idx.shape[0]):
+            point = points[i, j]
+            # log probs
+            log_probs = dists.log_prob(point)
+            probs = log_probs.sum(dim=-1).exp()
 
+            weighted_probs = probs * x0[i]
 
-
-
-
-    return None
+            x1[j] += weighted_probs.sum()
+    return x1
 
 
 
 if __name__ == '__main__':
-    num_input_units = 10
-    num_output_units = 5
+    num_input_units = 100
+    num_output_units = 100
     x0 = random_activation(num_input_units)
+    # x0 = torch.ones(num_input_units)
 
-    ticks = np.linspace(0, 1, num_input_units)
-    plt.figure()
-    sns.lineplot(ticks, x0)
+    palette = sns.color_palette()
+    plt.figure('input')
+    sns.barplot(np.round(np.linspace(0, 1, num_input_units), 2), x0, color=palette[0])
 
-    d, mu, sigma = generate_GMM(3)
-    plt.figure()
+    d, mu, sigma = generate_GMM(20)
     plot_gmm(d)
 
-    propagate(d, x0, num_output_units)
+    x1 = propagate(d, x0, num_output_units)
 
+    plt.figure('output')
+    sns.barplot(np.round(np.linspace(0, 1, num_output_units), 2), x1, color=palette[1])
+
+    x1_act = torch.nn.Tanh()(x1)
+
+    plt.figure('output after activation')
+    sns.barplot(np.round(np.linspace(0, 1, num_output_units), 2), x1_act, color=palette[1])
 
     plt.show()
 
